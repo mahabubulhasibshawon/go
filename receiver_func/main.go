97e1b40a3a38ab62d4650dc2f68a8ca581c6167c
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2025-03-08 23:45:09 +0600

    struct

--- receiver_func/main.go
+++ receiver_func/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+// struct
 type User struct {
 	Name string
 	Age  int
+}
+
+type Admin struct {
+	id   string
+	pass int
 }
 
 // receiver function
@@ -13,13 +19,26 @@ func (usr User) printDetails() {
 	fmt.Println("Age : ", usr.Age)
 }
 
+func (admin Admin) call(a int) {
+	fmt.Println("Id : ", admin.id)
+	fmt.Println("Pass : ", admin.pass)
+	fmt.Println(a)
+}
+
 func main() {
 	var user1 User
+	var admin1 Admin
 
 	user1 = User{
 		Name: "Hasib",
 		Age:  24,
 	}
 
+	admin1 = Admin{
+		id:   "rcv_func",
+		pass: 1234,
+	}
+
 	user1.printDetails()
+	admin1.call(88)
 }
