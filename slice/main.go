5e11ff5c880cad48e90fd22f44dd18eb076798e2
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2025-03-27 23:59:23 +0600

    slice

--- slice/main.go
+++ slice/main.go
@@ -12,6 +12,8 @@ func main() {
 	numbers := []int{1, 2, 3, 4, 5}
 	sliceExample()
 	fmt.Println(numbers)
+	slice := numbers[1:4]
+	fmt.Println("slice example : ", slice)
 	fmt.Printf("numbers has a data type of : %T\n", numbers)
 
 }
