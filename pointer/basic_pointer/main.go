42565c8d75cb077262135878a59590fff82b5921
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2025-05-04 23:27:29 +0600

    pointer

--- pointer/basic_pointer/main.go
+++ pointer/basic_pointer/main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	x := "value"
 	fmt.Println("1st value of x: ", x)
-	p := &x // address of pointer => &
-	*p = "changed value"
+	p := &x                            // address of pointer => &
+	*p = "value changed using pointer" // dereference pointer =>
 	fmt.Println("Address of p: ", p)
 
 	fmt.Println("changed value of x: ", x)
